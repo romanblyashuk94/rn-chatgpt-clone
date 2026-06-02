144c726c639cc41b6cdca698be87d2b39f6a3211
RomanBliashuk <rblyashuk@getrevscale.com>
2026-06-02 12:16:45 +0300

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import AnimatedIntro from "@/components/AnimatedIntro";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <AnimatedIntro />
+    </View>
+  );
+}
---
+++ components/AnimatedIntro.tsx
@@ -0,0 +1,248 @@
+import Colors from "@/constants/Colors";
+import { memo, useCallback, useState } from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import Animated, {
+  interpolate,
+  interpolateColor,
+  runOnJS,
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from "react-native-reanimated";
+
+const content = [
+  {
+    title: "Let's create.",
+    bg: Colors.lime,
+    fontColor: Colors.pink,
+  },
+  {
+    title: "Let's brainstorm.",
+    bg: Colors.brown,
+    fontColor: Colors.sky,
+  },
+  {
+    title: "Let's discover.",
+    bg: Colors.orange,
+    fontColor: Colors.blue,
+  },
+  {
+    title: "Let's go.",
+    bg: Colors.teal,
+    fontColor: Colors.yellow,
+  },
+  {
+    title: "ChatGPT.",
+    bg: Colors.green,
+    fontColor: Colors.pink,
+  },
+];
+
+const AnimatedIntro = () => {
+  const { width } = useWindowDimensions();
+  const ballWidth = 34;
+  const half = width / 2 - ballWidth / 2;
+
+  const currentX = useSharedValue(half);
+  const currentIndex = useSharedValue(0);
+  const isAtStart = useSharedValue(true);
+  const labelWidth = useSharedValue(0);
+  const canGoToNext = useSharedValue(false);
+  const didPlay = useSharedValue(false);
+
+  const [displayText, setDisplayText] = useState(content[0].title);
+
+  const updateText = useCallback((index: number) => {
+    setDisplayText(content[index].title);
+  }, []);
+
+  const newColorIndex = useDerivedValue(() => {
+    if (!isAtStart.value) {
+      return (currentIndex.value + 1) % content.length;
+    }
+    return currentIndex.value;
+  }, [currentIndex]);
+
+  const textStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        currentX.value,
+        [half, half + labelWidth.value / 2],
+        [
+          content[newColorIndex.value].fontColor,
+          content[currentIndex.value].fontColor,
+        ],
+        "RGB",
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            currentX.value,
+            [half, half + labelWidth.value / 2],
+            [half + 4, half - labelWidth.value / 2],
+          ),
+        },
+      ],
+    };
+  }, [currentIndex, currentX]);
+
+  const ballStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        currentX.value,
+        [half, half + labelWidth.value / 2],
+        [
+          content[newColorIndex.value].fontColor,
+          content[currentIndex.value].fontColor,
+        ],
+        "RGB",
+      ),
+      transform: [{ translateX: currentX.value }],
+    };
+  });
+
+  const mask = useAnimatedStyle(
+    () => ({
+      backgroundColor: interpolateColor(
+        currentX.value,
+        [half, half + labelWidth.value / 2],
+        [content[newColorIndex.value].bg, content[currentIndex.value].bg],
+        "RGB",
+      ),
+      transform: [{ translateX: currentX.value }],
+      width: width / 1.5,
+      borderTopLeftRadius: 20,
+      borderBottomLeftRadius: 20,
+    }),
+    [currentIndex, currentX, labelWidth],
+  );
+
+  const style1 = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(
+      currentX.value,
+      [half, half + labelWidth.value / 2],
+      [content[newColorIndex.value].bg, content[currentIndex.value].bg],
+      "RGB",
+    ),
+    opacity: interpolate(1, [1, 0], [1, 0, 0, 0, 0, 0, 0]),
+    transform: [
+      {
+        translateX: interpolate(
+          1,
+          [1, 0],
+          [0, -width * 2, -width, -width, -width, -width, -width],
+        ),
+      },
+    ],
+  }));
+
+  useAnimatedReaction(
+    () => currentIndex.value,
+    (index) => {
+      runOnJS(updateText)(index);
+    },
+  );
+
+  useAnimatedReaction(
+    () => labelWidth.value,
+    (newWidth) => {
+      if (newWidth === 0) return;
+      currentX.value = withDelay(
+        1000,
+        withTiming(
+          half + newWidth / 2,
+          {
+            duration: 800,
+          },
+          (finished) => {
+            if (finished) {
+              canGoToNext.value = true;
+              isAtStart.value = false;
+            }
+          },
+        ),
+      );
+    },
+    [labelWidth, currentX, half],
+  );
+
+  useAnimatedReaction(
+    () => canGoToNext.value,
+    (next) => {
+      if (next) {
+        canGoToNext.value = false;
+        currentX.value = withDelay(
+          1000,
+          withTiming(
+            half,
+            {
+              duration: 800,
+            },
+            (finished) => {
+              if (finished) {
+                currentIndex.value = (currentIndex.value + 1) % content.length;
+                isAtStart.value = true;
+                didPlay.value = false;
+              }
+            },
+          ),
+        );
+      }
+    },
+    [currentX, labelWidth],
+  );
+
+  return (
+    <Animated.View style={[styles.wrapper, style1]}>
+      <Animated.View style={[styles.content]}>
+        <Animated.View style={[styles.ball, ballStyle]} />
+        <Animated.View style={[styles.mask, mask]} />
+        <Animated.Text
+          onLayout={(e) => {
+            labelWidth.value = e.nativeEvent.layout.width + 4;
+          }}
+          style={[styles.title, textStyle]}
+        >
+          {displayText}
+        </Animated.Text>
+      </Animated.View>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  mask: {
+    zIndex: 1,
+    position: "absolute",
+    left: "0%",
+    height: 44,
+  },
+  ball: {
+    width: 40,
+    zIndex: 10,
+    height: 40,
+    backgroundColor: "#000",
+    borderRadius: 20,
+    position: "absolute",
+    left: "0%",
+  },
+  titleText: {
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "600",
+    left: "0%",
+    position: "absolute",
+  },
+  content: {
+    marginTop: 300,
+  },
+});
+export default memo(AnimatedIntro);
