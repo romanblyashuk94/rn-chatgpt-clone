39cdb50e36211b9911a946e1a1fa8e4f7141ca6b
RomanBliashuk <rblyashuk@getrevscale.com>
2026-06-05 17:13:57 +0300

    added zeego header dropdown

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { Text, View } from "react-native";
+import HeaderDropdown from "@/components/HeaderDropdown";
+import { defaultStyles } from "@/constants/Styles";
+import { Stack } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 const NewChatPage = () => {
+  const [gptVersion, setGptVersion] = useState<string>("3.5");
+
   return (
-    <View>
-      <Text>NewChatPage</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitle: () => (
+            <HeaderDropdown
+              title="Chat GPT"
+              items={[
+                { key: "3.5", title: "GPT-3.5", icon: "bolt" },
+                { key: "4", title: "GPT-4", icon: "sparkles" },
+              ]}
+              onSelect={(key) => {
+                setGptVersion(key);
+              }}
+              selected={gptVersion}
+            />
+          ),
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+});
 
 export default NewChatPage;
--- app/(auth)/(drawer)/dalle.tsx
+++ app/(auth)/(drawer)/dalle.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { Text, View } from "react-native";
+import HeaderDropdown from "@/components/HeaderDropdown";
+import { defaultStyles } from "@/constants/Styles";
+import { Stack } from "expo-router";
+import { View } from "react-native";
 
 const DallePage = () => {
   return (
-    <View>
-      <Text>DallePage</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitle: () => (
+            <HeaderDropdown
+              title="Dall·E"
+              items={[
+                {
+                  key: "share",
+                  title: "Share GPT",
+                  icon: "square.and.arrow.up",
+                },
+                { key: "details", title: "See Details", icon: "info.circle" },
+                { key: "keep", title: "Keep in Sidebar", icon: "pin" },
+              ]}
+              onSelect={() => {}}
+              selected={undefined}
+            />
+          ),
+        }}
+      />
     </View>
   );
 };
