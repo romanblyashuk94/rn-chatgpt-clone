1706bca728d7e363959f1a4ad4f2854912c3c88a
RomanBliashuk <rblyashuk@getrevscale.com>
2026-07-09 17:50:04 +0300

    added content to explore screen

--- app/(auth)/(drawer)/explore.tsx
+++ app/(auth)/(drawer)/explore.tsx
@@ -6,15 +6,21 @@ import {
   useHeaderHeight,
 } from "@react-navigation/elements";
 import { BlurView } from "expo-blur";
+import { LinearGradient } from "expo-linear-gradient";
 import Drawer from "expo-router/drawer";
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import {
+  Image,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import { createShimmerPlaceholder } from "react-native-shimmer-placeholder";
+
+const ShimmerPlaceholder = createShimmerPlaceholder(LinearGradient);
 
 const sections = [
   { title: "Top Picks", label: "Curated top picks from this week" },
@@ -32,9 +38,61 @@ const sections = [
   { title: "Programming", label: "Write code, debug, test, and learn" },
 ];
 
+const apps = [
+  {
+    title: "Instant Website [Multipage]",
+    description:
+      "Generates functional multipage websites aimed at meeting the needs of startups and small businesses. Continuously updated with new features.",
+    author: "By Max & Kirill Dubovitsky",
+    image:
+      "https://files.oaiusercontent.com/file-9P4NhIxlr14rKlHEM41VVbxS?se=2124-03-21T08%3A51%3A45Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D1209600%2C%20immutable&rscd=attachment%3B%20filename%3Dsdfgasdfx.jpg&sig=8xHssdF2qgY0qpyaUNRn4My5tJwh9iYMn1Lg53H9Z1c%3D",
+  },
+  {
+    title: "Diagrams & Data",
+    description:
+      "Helps research, analyze, and visualize complex data through diagrams and charts. Useful for coders and business analysts alike.",
+    author: "By Max & Kirill Dubovitsky",
+    image:
+      "https://files.oaiusercontent.com/file-teufH6uVdqxmxHjEUIQjD8ur?se=2124-03-24T19%3A02%3A04Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D1209600%2C%20immutable&rscd=attachment%3B%20filename%3Dvar6.jpg&sig=wn6KyKdgbqJ1gGkHltYV8cl3/ZwLZmgO039GkueA8Z8%3D",
+  },
+  {
+    title: "ChatPRD",
+    description:
+      "Acts as an on-demand Chief Product Officer, enhancing product requirement documents and providing coaching for product managers and engineers.",
+    author: "By Claire V Lawless",
+    image:
+      "https://files.oaiusercontent.com/file-qeVpUG3AJT0FINT4eZ6Gbt2q?se=2123-10-17T23%3A41%3A20Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dcvolawless_illustration_of_a_female_ceo_at_a_laptop_lo-fi_asthe_c60ce7fb-5902-474c-aa85-54c7469aa089.png&sig=eHU4/LmvHg96KaqivlhaLufaIleMC1wm3pE0kMQF1AA%3D",
+  },
+  {
+    title: "Music Teacher",
+    description:
+      "Specializes in music theory, scales, production, and more, also includes image generation capabilities for cover art.",
+    author: "By gryphonedm.com",
+    image:
+      "https://files.oaiusercontent.com/file-gLZOuk6mmgg4vsCuhWxgQ2Cm?se=2123-12-13T21%3A57%3A04Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D1209600%2C%20immutable&rscd=attachment%3B%20filename%3D3e763913-0301-49ec-b2f0-c9ad832df862.png&sig=cCp02Ji5dcCxt0UPu92vrFgmZ8p1jwkqUYlYbR4IfoY%3D",
+  },
+  {
+    title: "UX Design Mentor",
+    description:
+      "Provides specific feedback on UX or Product Design, enhancing the design process.",
+    author: "By community builder",
+    image:
+      "https://files.oaiusercontent.com/file-Nz98JJBUj7rzXmJAyALeuV87?se=2123-10-16T04%3A49%3A04Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D476c2153-1121-4e4c-ad3b-ea164ec21499.png&sig=8TkhKoq6xPQYcrXHiCzvp2SV0G9k3jMllTAy3fe30R8%3D",
+  },
+];
+
 const ExplorePage = () => {
   const headerHeight = useHeaderHeight();
   const [selected, setSelected] = useState(sections[0].title);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -100,7 +158,73 @@ const ExplorePage = () => {
         contentContainerStyle={{
           paddingTop: headerHeight,
         }}
-      ></ScrollView>
+      >
+        {sections.map((section, index) => (
+          <Fragment key={index}>
+            {selected === section.title && (
+              <Animated.View
+                entering={FadeIn.duration(400).delay(300)}
+                exiting={FadeOut.duration(300)}
+                style={styles.section}
+              >
+                <ShimmerPlaceholder width={160} height={20} visible={!loading}>
+                  <Text style={styles.title}>{section.title}</Text>
+                </ShimmerPlaceholder>
+
+                <ShimmerPlaceholder
+                  width={280}
+                  height={20}
+                  visible={!loading}
+                  shimmerStyle={{ marginVertical: 10 }}
+                >
+                  <Text style={styles.label}>{section.label}</Text>
+                </ShimmerPlaceholder>
+
+                {apps.map((app, index) => (
+                  <View key={index} style={styles.card}>
+                    <ShimmerPlaceholder
+                      width={60}
+                      height={60}
+                      visible={!loading}
+                      shimmerStyle={{ borderRadius: 30 }}
+                    >
+                      <Image
+                        source={{ uri: app.image }}
+                        style={styles.cardImage}
+                      />
+                    </ShimmerPlaceholder>
+
+                    <View style={{ flexShrink: 1, gap: 4 }}>
+                      <ShimmerPlaceholder
+                        width={160}
+                        height={20}
+                        visible={!loading}
+                      >
+                        <Text style={styles.cardTitle}>{app.title}</Text>
+                      </ShimmerPlaceholder>
+                      <ShimmerPlaceholder
+                        width={160}
+                        height={20}
+                        visible={!loading}
+                        shimmerStyle={{ marginVertical: 10 }}
+                      >
+                        <Text style={styles.cardDesc}>{app.description}</Text>
+                      </ShimmerPlaceholder>
+                      <ShimmerPlaceholder
+                        width={250}
+                        height={20}
+                        visible={!loading}
+                      >
+                        <Text style={styles.cardAuthor}>{app.author}</Text>
+                      </ShimmerPlaceholder>
+                    </View>
+                  </View>
+                ))}
+              </Animated.View>
+            )}
+          </Fragment>
+        ))}
+      </ScrollView>
     </View>
   );
 };
